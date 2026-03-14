67e9f4da180f1f4e1d88e327ab5b115fbefa92ee
perandowind <perandowind@gmail.com>
2026-03-14 10:48:10 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 94.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0, len = signs.length;
+        for(int i=0; i<len; i++){
+            if(signs[i] == true) answer += absolutes[i];
+            else answer -= absolutes[i];
+        }
+        
+        return answer;
+    }
+}
