bf4ecf561704ed18eb4aeb18c7a02553b7742c6b
perandowind <perandowind@gmail.com>
2026-03-14 11:26:37 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,16 +1,15 @@
 class Solution {
     public int solution(long num) {
         int count = 0;
-        if(num == 1) return 0;
-        while(true){
+        
+        while(num != 1 && count < 500){
             count++;
-            if(num % 2 == 0) num /= 2;
-            else num = num * 3 + 1;
+            num = (num % 2 == 0) ? num/2 : 3*num + 1;
             
-            if(num == 1)  break;
-            if(count == 500) return -1;
+            if(num == 1)  return count;
+            
         }
         
-        return count;
+        return count >= 500 ? -1 : count;
     }
 }
