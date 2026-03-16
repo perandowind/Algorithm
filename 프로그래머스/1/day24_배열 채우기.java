8c60b85539470a3f409642a6193622f8aa70bb44
perandowind <perandowind@gmail.com>
2026-03-16 20:50:42 +0900

    day24_배열 채우기
    프로그래머스 코테 day24_배열 채우기

--- 프로그래머스/1/day24_배열 채우기.java
+++ 프로그래머스/1/day24_배열 채우기.java
@@ -20,3 +20,24 @@ class Solution {
         return stack.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+
+// Stack을 사용하면 오름차순 그대로 배열변환 가능
+// class Solution {
+//     public int[] solution(int[] arr, int k) {
+//         Stack<Integer> stack = new Stack<>();
+//         stack.push(arr[0]);
+//         int index = 1;
+        
+//         while(index < arr.length){
+//             if(arr[index] - stack.peek() > k) {
+//                 stack.push(stack.peek() + k);
+//                 continue;
+//             }else{
+//                 stack.push(arr[index]);
+//                 index++;
+//             }
+//         }
+        
+//         return stack.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
