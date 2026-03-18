08782d919c07239722a923c7e1ada3b4f68a490d
perandowind <perandowind@gmail.com>
2026-03-18 14:52:09 +0900

    [level 1] Title: 예산, Time: 1.14 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,16 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int left = budget;
         
         Arrays.sort(d);
         
         if(d[0] > budget) return 0;
         
         for(int i=0; i<d.length; i++) {
-            if(left >= d[i]){
+            if(budget >= d[i]){
                 answer++;
-                left -= d[i];
+                budget -= d[i];
             }else break;
             
         }
