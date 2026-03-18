f15689492ecd91f8093f60ee8a790441c3e1a81a
perandowind <perandowind@gmail.com>
2026-03-18 11:43:25 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.60 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,24 +1,13 @@
 class Solution {
     public String solution(String s) {
-        int len = s.length();
-        int index = 0;
         StringBuilder sb = new StringBuilder();
-        
-        for(int i=0; i<len; i++){
-            if(s.charAt(i) == ' '){
-                sb.append(' ');
-                index = 0;
-                continue;
-            }
-            if(index % 2 == 0) {
-                sb.append(String.valueOf(s.charAt(i)).toUpperCase());
-                index++;
-            }else if(index % 2 == 1) {
-                sb.append(String.valueOf(s.charAt(i)).toLowerCase());
-                index++;
-            }
+        String[] arr = s.split("");
+        int cnt = 0;
+        for(String str : arr){
+            cnt = str.equals(" ") ? 0 : cnt + 1;
+            String input = cnt % 2 == 1 ? str.toUpperCase() : str.toLowerCase();
+            sb.append(input);
         }
-            
             
         return sb.toString();
     }
