7c7fceedfccc4f9c515abb2fb07365179cfdc0e8
perandowind <perandowind@gmail.com>
2026-03-29 22:22:37 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0; 
-        int tmp = 0;
-        while(n + tmp >= a){ 
-            int val = ((n + tmp) / a) * b;
-            tmp = (n + tmp) % a;
-            n = val;
-            answer += n;
+
+        while(n >= a){ 
+            answer += (n / a) * b;
+            n = (n / a) * b + (n % a);
         }
         
         return answer;
