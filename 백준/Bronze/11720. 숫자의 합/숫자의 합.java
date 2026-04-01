8ba7026b305eda45a089b2521372254323e5ce33
perandowind <perandowind@gmail.com>
2026-04-01 16:26:44 +0900

    [Bronze IV] Title: 숫자의 합, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -5,7 +5,6 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
         String num = sc.next();
-        int length = Math.min(count, num.length());
         int sum = 0;
         for(int i=0; i<count; i++){
             sum += num.charAt(i) - '0';
