f6ccf83860aa451ef2689178f176ae049268d83e
perandowind <perandowind@gmail.com>
2026-04-02 23:38:20 +0900

    [level 1] Title: 추억 점수, Time: 6.00 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,13 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0; i<name.length; i++){
+            map.put(name[i], yearning[i]);
+        }
         
         for(int i=0; i<photo.length; i++){
             int sum = 0;
-            for(String s : photo[i]){
-                for(int j=0; j<name.length; j++){
-                    if(name[j].equals(s)) sum += yearning[j];
-                }
+            for(int j=0; j<photo[i].length; j++){
+                String str = photo[i][j];
+                sum += map.getOrDefault(str, 0);
             }
             answer[i] = sum;
         }
@@ -15,3 +20,21 @@ class Solution {
         return answer;
     }
 }
+
+// class Solution {
+//     public int[] solution(String[] name, int[] yearning, String[][] photo) {
+//         int[] answer = new int[photo.length];
+        
+//         for(int i=0; i<photo.length; i++){
+//             int sum = 0;
+//             for(String s : photo[i]){
+//                 for(int j=0; j<name.length; j++){
+//                     if(name[j].equals(s)) sum += yearning[j];
+//                 }
+//             }
+//             answer[i] = sum;
+//         }
+        
+//         return answer;
+//     }
+// }
