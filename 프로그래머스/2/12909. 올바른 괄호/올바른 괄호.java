e9c8c2a41b2c061ee782c484c9f1beadfbab3d40
perandowind <perandowind@gmail.com>
2026-04-04 16:37:25 +0900

    [level 2] Title: 올바른 괄호, Time: 46.94 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,6 +1,5 @@
 class Solution {
     boolean solution(String s) {
-        if(s.charAt(0) == ')' || s.charAt(s.length()-1) == '(') return false;
         int answer = 0;
         for(String str : s.split("")){
             if(answer < 0) return false;
