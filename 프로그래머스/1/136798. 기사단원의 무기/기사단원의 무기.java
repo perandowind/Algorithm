633c0184bd15d632fe4dae92cd0411da3178f45b
perandowind <perandowind@gmail.com>
2026-04-06 17:27:50 +0900

    [level 1] Title: 기사단원의 무기, Time: 11.67 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -2,19 +2,39 @@ class Solution {
 
     public int solution(int number, int limit, int power) {
         int[] count = new int[number + 1];    
-        for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= number / i; j++) {
-                count[i * j]++;
+        for(int i=1; i<=number; i++){
+            for(int j=i; j<=number; j += i){
+                count[j]++;
             }
         }
+        
         int answer = 0;
-        for (int i = 1; i <= number; i++) {
-            if (count[i] > limit) {
-                answer += power;
-            } else {
-                answer += count[i];
-            }
+        for(int i=1; i<count.length; i++){
+            answer += count[i] <= limit ? count[i] : power;
         }
+        
         return answer;
     }
 }
+
+// 기존 약수의 개수 방식
+// class Solution {
+
+//     public int solution(int number, int limit, int power) {
+//         int[] count = new int[number + 1];    
+//         for (int i = 1; i <= number; i++) {
+//             for (int j = 1; j <= number / i; j++) {
+//                 count[i * j]++;
+//             }
+//         }
+//         int answer = 0;
+//         for (int i = 1; i <= number; i++) {
+//             if (count[i] > limit) {
+//                 answer += power;
+//             } else {
+//                 answer += count[i];
+//             }
+//         }
+//         return answer;
+//     }
+// }
