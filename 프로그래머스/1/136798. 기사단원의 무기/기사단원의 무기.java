d5306c51f0a5a99449eea7174293bcd2d018b438
perandowind <perandowind@gmail.com>
2026-04-06 17:15:14 +0900

    [level 1] Title: 기사단원의 무기, Time: 12.46 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,19 +1,20 @@
 class Solution {
+
     public int solution(int number, int limit, int power) {
+        int[] count = new int[number + 1];    
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
+            }
+        }
         int answer = 0;
-        for(int i=1; i<=number; i++){
-            int div = divisors(i); // 약수의 개수
-            answer += div <= limit ? div : power;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
         }
         return answer;
-    }
-    
-    int divisors(int num){
-        int cnt = 0;
-        for(int i=1; i*i <= num; i++){
-            if(i*i == num) cnt++;
-            else if(num % i == 0) cnt += 2;
-        }
-        return cnt;
     }
 }
