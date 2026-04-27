82865492086cb1c18f3b8df60e30269c0d425816
perandowind <perandowind@gmail.com>
2026-04-27 16:21:38 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -2,10 +2,10 @@ class Solution {
     public int solution(int[] number) {
         int answer = 0, len = number.length;
         
-        for(int i=0; i<len-2; i++){
+        for(int i=0; i<len; i++){
             for(int j=i+1; j<len; j++){
                 for(int n=j+1; n<len; n++){
-                    if(number[i] + number[j] + number[n] == 0) answer++;
+                    if(number[i]+number[j]+number[n] == 0) answer++;
                 }
             }
         }
