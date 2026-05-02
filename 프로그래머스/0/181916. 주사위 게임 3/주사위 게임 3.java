cdc77ee76274cf6e06e977b08fd70615b66c6115
perandowind <perandowind@gmail.com>
2026-05-02 22:46:36 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.13 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int answer = 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(a, map.getOrDefault(a, 0) + 1);
+        map.put(b, map.getOrDefault(b, 0) + 1);
+        map.put(c, map.getOrDefault(c, 0) + 1);
+        map.put(d, map.getOrDefault(d, 0) + 1);
+
+        if(map.size() == 1){
+            int p = a;
+            answer = 1111 * p;
+        }else if(map.size() == 2){
+            int p = 0;
+            int q = 0;
+            if(map.get(a) == 3 || map.get(a) == 1){
+                for(int key : map.keySet()){
+                    if(map.get(key) == 3) p = key;
+                    else q = key;
+                } answer = (10*p+q)*(10*p+q);
+            } else{
+                p = a;
+                q = a == b ? c : b;
+                answer = (p+q) * Math.abs(p-q);
+            }
+        }else if(map.size() == 3){
+            for(int key : map.keySet()){
+                if(map.get(key) == 1) answer *= key;
+            }
+        }else{
+            answer = Math.min(Math.min(a,b), Math.min(c,d));
+        }
+        
+        return answer;
+    }
+}
