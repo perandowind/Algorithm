c150aad612c8f4bfe22ad23ba0ef6056d30f202a
perandowind <perandowind@gmail.com>
2026-05-03 01:37:24 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -9,16 +9,16 @@ class Solution {
             x4 = dots[3][0], y4 = dots[3][1];
         
         
-        double incline1 = (double)(x1-x2) / (y1-y2);
-        double incline2 = (double)(x3-x4) / (y3-y4);
+        int incline1 = (x1-x2) * (y3-y4);
+        int incline2 = (x3-x4) * (y1-y2);
         if(incline1 == incline2) return 1;
         
-        incline1 = (double)(x1-x3) / (y1-y3);
-        incline2 = (double)(x2-x4) / (y2-y4);
+        incline1 = (x1-x3) * (y2-y4);
+        incline2 = (x2-x4) * (y1-y3);
         if(incline1 == incline2) return 1;
         
-        incline1 = (double)(x1-x4) / (y1-y4);
-        incline2 = (double)(x2-x3) / (y2-y3);
+        incline1 = (x1-x4) * (y2-y3);
+        incline2 = (x2-x3) * (y1-y4);
         if(incline1 == incline2) return 1;
         
         return answer;
