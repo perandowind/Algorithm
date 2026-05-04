d71b3b0ca5903182e4e233c9c6fece50d565fa7a
perandowind <perandowind@gmail.com>
2026-05-05 08:59:37 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.78 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        
+        String[][] map = new String[n][n];
+        
+        for(int i=0; i<n; i++){
+            String bin = Integer.toUnsignedString(arr1[i], 2); // "1001"
+            bin = bin.length() < n ? "0".repeat(n-bin.length()) + bin : bin; // "01001"
+            String[] temp = bin.split(""); // ["0","1","0","0","1"]
+            for(int j=0; j<temp.length; j++){
+                map[i][j] = temp[j].equals("1") ? "#" : " ";
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            String bin = Integer.toUnsignedString(arr2[i], 2); // "1001"
+            bin = bin.length() < n ? "0".repeat(n-bin.length()) + bin : bin; // "01001"
+            String[] temp = bin.split(""); // ["0","1","0","0","1"]
+            for(int j=0; j<temp.length; j++){
+                if(!map[i][j].equals("#")){
+                    map[i][j] = temp[j].equals("1") ? "#" : " ";
+                }
+            }
+        }
+        
+        String[] answer = new String[n];
+        
+        
+        for(int i=0; i<n; i++){
+            StringBuilder sb = new StringBuilder();    
+            for(int j=0; j<n; j++){
+                sb.append(map[i][j]);
+            }
+            answer[i] = sb.toString();
+        }
+        
+        return answer;
+    }
+}
