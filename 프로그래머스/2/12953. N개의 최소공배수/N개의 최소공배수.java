fed1eb271d275ad8e73c1614600ce6db8ebc2571
perandowind <perandowind@gmail.com>
2026-05-12 22:43:10 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,28 @@
+class Solution {
+    // 2^10 = 1024 * 32 = 32000
+    // lcm = b * (a/gcd)
+    public int solution(int[] arr) {
+
+        int lcm = lcm(arr[0], arr[1]);
+        
+        for(int i=2; i<arr.length; i++){
+            lcm = lcm(lcm, arr[i]);
+        }
+        
+        return lcm;
+    }
+    
+    int lcm(int a, int b){
+        int gcd = gcd(a, b);
+        return a * (b / gcd);
+    }
+    
+    int gcd(int a, int b){
+        while(b>0){
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
